ce7ace5a8f1bb3d4d63f3eae6fe55adf6ac8ffee
nerdynischal <nerdynischal@gmail.com>
2025-05-29 14:11:29 +0100

    Refactor main.js

--- main.js
+++ main.js
@@ -1,63 +1,81 @@
 let penColor = "black";
 
+function changePenColor(color) {
+  penColor = color;
+}
+
+/* Create grid on board */
 function populateBoard(size) {
   let board = document.querySelector(".board");
 
   let squares = board.querySelectorAll("div");
-  squares.forEach((div) => div.remove());
+  squares.forEach((div) => div.remove()); //remove previous grid
 
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
   boardSize = size * size;
 
   for (let i = 0; i < boardSize; i++) {
     let square = document.createElement("div");
-    square.addEventListener("mouseover", colorSquare);
+    square.addEventListener("mouseover", colorGrid);
     square.classList.add("square");
     board.insertAdjacentElement("beforeend", square);
   }
 }
 
-populateBoard(16);
-
+/* Change grid size */
 function changeSize(input) {
   if (input >= 2 || input <= 100) {
     populateBoard(input);
-    console.log(input);
   } else {
-    console.log("too many... ");
+    console.log("Please enter a value between 2 and 100.");
   }
 }
 
-function colorSquare() {
+/* Draw colour on grid */
+function colorGrid() {
   if (penColor === "random") {
     let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    console.log(randomColor);
     this.style.backgroundColor = "#" + randomColor;
   } else {
     this.style.backgroundColor = penColor;
   }
 }
 
-function changeColor(choice) {
-  penColor = choice;
-}
-
+/* Clear the board */
 function clearBoard() {
   let board = document.querySelector(".board");
   let squares = board.querySelectorAll("div");
   squares.forEach((div) => (div.style.backgroundColor = "rgb(215, 239, 247)"));
 }
 
+/* Get black button and add functionality */
 const buttonBlack = document.querySelector(".btn-black");
-const buttonRandom = document.querySelector(".btn-rainbow");
+buttonBlack.addEventListener("click", () => {
+  changePenColor("black");
+  toggleSelected(buttonBlack);
+});
+
+/* Get random button and add functionality */
+const buttonRandom = document.querySelector(".btn-random");
+buttonRandom.addEventListener("click", () => {
+  changePenColor("random");
+  toggleSelected(buttonRandom);
+});
+
+/*Get eraser button and add functionality */
 const buttonEraser = document.querySelector(".btn-eraser");
+buttonEraser.addEventListener("click", () => {
+  changePenColor(`rgb(215, 239, 247)`);
+  toggleSelected(buttonEraser);
+});
+
+/*Get clear button and add functionality */
+const buttonClear = document.querySelector(".btn-clear");
+buttonClear.addEventListener("click", clearBoard());
+
+/*Toggle selected indicator on buttons */
 const selectedDivs = document.querySelectorAll(".selected");
-
-const btnBlackIndicator = buttonBlack.querySelector(".selected");
-btnBlackIndicator.style.display = "inline";
-
 function toggleSelected(btn) {
   selectedDivs.forEach((div) => {
     if (div.parentElement === btn) {
@@ -68,37 +86,28 @@ function toggleSelected(btn) {
   });
 }
 
-buttonBlack.addEventListener("click", () => {
-  changeColor("black");
-  toggleSelected(buttonBlack);
-});
-
-buttonRandom.addEventListener("click", () => {
-  changeColor("random");
-  toggleSelected(buttonRandom);
-});
-
-buttonEraser.addEventListener("click", () => {
-  changeColor(`rgb(215, 239, 247)`);
-  toggleSelected(buttonEraser);
-});
-
+/* Add keyboard functionality */
 window.onkeydown = function (e) {
   console.log(e.key);
 
   if (e.key === "b") {
-    changeColor("black");
+    changePenColor("black");
     toggleSelected(buttonBlack);
   }
   if (e.key === "r") {
-    changeColor("random");
+    changePenColor("random");
     toggleSelected(buttonRandom);
   }
   if (e.key === "e") {
-    changeColor(`rgb(215, 239, 247)`);
+    changePenColor(`rgb(215, 239, 247)`);
     toggleSelected(buttonEraser);
   }
   if (e.key === "c") {
     clearBoard();
   }
 };
+
+/* Create board with default values */
+populateBoard(16);
+const btnBlackIndicator = buttonBlack.querySelector(".selected");
+btnBlackIndicator.style.display = "inline";
